493f3702438625e01582480a8679ea578f9c6a60
Brandon Ortiz <ortizbrandon140@gmail.com>
2024-10-24 15:02:42 -0500

    brandon/shooting and item pick up complete

--- Assets/Brandon/Scripts/PlayerMovement.cs
+++ Assets/Brandon/Scripts/PlayerMovement.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.TextCore.Text;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -16,9 +18,10 @@ public class PlayerMovement : MonoBehaviour
     public Transform item; 
     public Rigidbody2D bulletPrefab;
     public Transform bulletSpawn;
-    private float bulletSpeed = 6;
+    private float bulletSpeed = 7;
     private int shots = 5;
     public TextMeshProUGUI text; 
+    private Vector2 directionShot;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
         // updating the shots counter
         text.gameObject.SetActive(itemInHand);
         text.gameObject.GetComponent<TextMeshProUGUI>().text = "Shots = " + shots;
+        item.gameObject.SetActive(!itemInHand);
 
         //transform.position = new Vector3(transform.position.x + movement.x, transform.position.y + movement.y, 0f);
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + movement.x, transform.position.y + movement.y, 0f), speed * Time.deltaTime);
@@ -43,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
 
         if(item.position.x - 0.5f < gameObject.transform.position.x && item.position.x + 0.5f > gameObject.transform.position.x &&
         item.position.y - 0.5f < gameObject.transform.position.y && item.position.y + 0.5f > gameObject.transform.position.y &&
-        Input.GetKeyDown(KeyCode.Space)){
+        Input.GetKeyDown(KeyCode.Space) && item.gameObject.activeSelf){
             itemInHand = true;
             shots = 5;
             gameObject.GetComponent<SpriteRenderer>().color = Color.white;
@@ -55,6 +59,7 @@ public class PlayerMovement : MonoBehaviour
     
     void OnMove(InputValue val){
         movement = val.Get<Vector2>();
+        if(movement != Vector2.zero)directionShot = movement;
     }
 
         private void Shoot(){
@@ -63,19 +68,15 @@ public class PlayerMovement : MonoBehaviour
             Rigidbody2D bull = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
 
             Vector2 Vel = rb.velocity;
-            Vector2 Dir;
-            if(Input.GetKeyDown(KeyCode.S)) Dir = -transform.up;
-            if(Input.GetKeyDown(KeyCode.A)) Dir = -transform.right;
-            if(Input.GetKeyDown(KeyCode.D)) Dir = transform.right;
-            else Dir = transform.forward;
+            Vector2 Dir = transform.up;
 
             float forwardSpeed = Vector2.Dot(Vel, Dir);
 
             if(forwardSpeed < 0){
-                forwardSpeed= 0;
+                forwardSpeed = 0;
             }
             bull.velocity = Dir * forwardSpeed;
-            bull.AddForce(bulletSpeed * transform.up, ForceMode2D.Impulse);
+            bull.AddForce(bulletSpeed * directionShot, ForceMode2D.Impulse);
             shots--;
         }
     }
